0e22399d3c461a9663b654bd07de67c669f0733e
Atharva Thakur <atharvathakur369@gmail.com>
2023-09-21 00:47:02 +0530

    AWS acc migration - App - version 1

--- flavour-finder-app/src/index.js
+++ flavour-finder-app/src/index.js
@@ -4,22 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import config from './aws-exports';
-// import Amplify from 'aws-amplify';
-import { Amplify } from 'aws-amplify';
-
-// Now you can use Amplify in your code
-
-import { AmplifyProvider } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-
-Amplify.configure(config)
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AmplifyProvider>
+  <React.StrictMode>
     <App />
-    </AmplifyProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
